05cc771c4bb8f7dc8075806e27d865f64b37b5bf
Yara1209 <160124985+Yara1209@users.noreply.github.com>
2025-10-10 10:41:33 -0300

    yarinha

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -7,5 +8,6 @@ for (let i = 0; i < botoes.length; i++) {
     }
 
     botoes[i].classList.add("ativo");
+    textos[j].classList.remove("ativo");
   };
 }webview
